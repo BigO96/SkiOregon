0f48b5c0f2ae48cb3b1e1fe23ecc4b2b440e17af
Oscar Epp <oepp@uoregon.edu>
2024-04-13 00:56:23 -0700

    Initial Commit

---
+++ SkiOregon/SkiOregonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkiOregonApp.swift
+//  SkiOregon
+//
+//  Created by Oscar Epp on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SkiOregonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
